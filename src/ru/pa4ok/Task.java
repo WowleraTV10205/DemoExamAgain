9ab9427cdab2b4544d677a5a3c8dc27092a750a8
Pa4ok <n_leontiev@mail.ru>
2021-03-11 13:49:43 +0300

    11.03.21-2

---
+++ src/ru/pa4ok/Task.java
@@ -0,0 +1,36 @@
+package ru.pa4ok;
+
+public class Task
+{
+    /*
+        Library
+        - String title
+        - String address
+        - List<Book> books
+        - Set<Author> authors
+        - public void addBook(Book book)
+            добавить книгу в список
+            если в authors еще не было такого автора
+            то вывести в консоль информацию о нем
+            и добавить его в authors
+        - public List<Book> getAuthorBooks(Author author)
+            метод должен вернуть список всех книг автора
+        - public int deleteAuthorBooks(Author author)
+            метод должен удалить все книги автора
+            и самого автора из сета
+            возвращаемое значение - колво удаленных книг
+
+        Author
+        - String name
+
+        Book
+        - String title
+        - Author author
+        - int pages
+     */
+
+    public static void main(String[] args)
+    {
+
+    }
+}
