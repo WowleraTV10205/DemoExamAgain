0771acf0c4b66fe9eaabde5299702d953c746345
Pa4ok <n_leontiev@mail.ru>
2021-03-13 16:52:07 +0300

    13.03.21

---
+++ src/ru/pa4ok/Book.java
@@ -0,0 +1,99 @@
+package ru.pa4ok;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Book
+{
+    private int id;
+    private String title;
+    private String author;
+    private Date createDate;
+    private int pages;
+    private String shortDesc;
+
+    public Book(int id, String title, String author, Date createDate, int pages, String shortDesc) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.createDate = createDate;
+        this.pages = pages;
+        this.shortDesc = shortDesc;
+    }
+
+    public Book() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id && pages == book.pages && Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(createDate, book.createDate) && Objects.equals(shortDesc, book.shortDesc);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author, createDate, pages, shortDesc);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", createDate=" + createDate +
+                ", pages=" + pages +
+                ", shortDesc='" + shortDesc + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    public void setShortDesc(String shortDesc) {
+        this.shortDesc = shortDesc;
+    }
+}
--- src/ru/pa4ok/Main.java
+++ src/ru/pa4ok/Main.java
@@ -1,110 +1,45 @@
 package ru.pa4ok;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.*;
 
 public class Main
 {
+
     /*
         ALT + INSERT - автогенерация (алт. пр. кнопка мышки -> generate)
         ALT + ENTER - вызов меню с возможными решениями ошибки
      */
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws SQLException
     {
-        List<TestClass> list = new ArrayList<>();
+        /*System.out.println(BookManager.add(new Book(
+                -1,
+                "test title",
+                "test author",
+                new Date(),
+                228,
+                "test desc"
+        )));*/
 
-        for(int i=0; i<10; i++) {
-            list.add(new TestClass());
-        }
+        //System.out.println(BookManager.getById(3));
 
-        System.out.println(list);
+        //System.out.println(BookManager.getAll());
 
-        //вариант 1, не требует ничего дополнительного
-        /*Collections.sort(list, new Comparator<TestClass>() {
-            @Override
-            public int compare(TestClass o1, TestClass o2) {
-                //-1 o1 < 02
-                //0 o1 == o2
-                //1 o1 > o2
-                return Integer.compare(o1.getI(), o2.getI());
-            }
-        });
 
-        //одинаковый код с тем что выше
-        Collections.sort(list, (o1, o2) -> Integer.compare(o1.getI(), o2.getI()));
+        /*Book book = BookManager.getById(3);
+        book.setAuthor("new author pirgoirhgoegrgerwi");
+        book.setCreateDate(new Date());
+        System.out.println(BookManager.update(book));*/
 
-        //опять одинаковый
-        Collections.sort(list, Comparator.comparingInt(TestClass::getI));*/
-
-        //вариант 2
-        //класс, который хранится в колекции
-        //должен реализовавывать интерфейс Comparable
-        Collections.sort(list);
-
-        /*
-            2 метода
-            1 более подходит если у вас много различных правил для сортировки
-            2 подходит если колекция с выбранным типом всегда сортируется одинаково
-         */
-
-        System.out.println(list);
-    }
-}
-
-class TestClass implements Comparable<TestClass>
-{
-    private int i;
-
-    public TestClass(int i) {
-        this.i = i;
+        System.out.println(BookManager.deleteById(2));
     }
 
-    public TestClass() {
-        this(new Random().nextInt(1000));
-    }
-
-    @Override
-    public int compareTo(TestClass o) {
-        return Integer.compare(this.i, o.getI());
-    }
-
-    /*
-
-        Так как мы не можем сравнить сложные объекты через ==
-        нам нужно сравнивать их через equals
-        для ваших классов вы можете через ALT + INSERT
-        сгенерировать готовые методы для сравнения с учетом полей
-
-     */
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TestClass testClass = (TestClass) o;
-        return i == testClass.i;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(i);
-    }
-
-    //
-
-    @Override
-    public String toString() {
-        return "TestClass{" +
-                "i=" + i +
-                '}';
-    }
-
-    public int getI() {
-        return i;
-    }
-
-    public void setI(int i) {
-        this.i = i;
+    //"короткий" способ подключения к базе
+    public static Connection getConnection() throws SQLException
+    {
+        return DriverManager.getConnection("jdbc:mysql://116.202.236.174:3306/DemoExam?serverTimeZone=UTC", "DemoExam","12345");
     }
 }
