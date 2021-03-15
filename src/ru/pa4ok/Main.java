047fa6907c8b4b015367636210175ead018fe7bd
Pa4ok <n_leontiev@mail.ru>
2021-03-15 16:20:46 +0300

    15.03.21

--- src/ru/pa4ok/Main.java
+++ src/ru/pa4ok/Main.java
@@ -1,140 +1,37 @@
 package ru.pa4ok;
 
-import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Date;
 
 public class Main
 {
-    /*
-        Library
-        - String title
-        - String address
-        - List<Book> books
-        - Set<Author> authors
-        - public void addBook(Book book)
-            добавить книгу в список
-            если в authors еще не было такого автора
-            то вывести в консоль информацию о нем
-            и добавить его в authors
-        - public List<Book> getAuthorBooks(Author author)
-            метод должен вернуть список всех книг автора
-        - public int deleteAuthorBooks(Author author)
-            метод должен удалить все книги автора
-            и самого автора из сета
-            возвращаемое значение - колво удаленных книг
-        - public void sortBooksByPages(boolean reverse)
-            сортирует books, если reverse == false, то от меньшего к большему
-            если reverse == true - наоборот
-        - public void sortBooksByTitle()
-            сортирует книги по алфавиту (название)
-        - public void shuffle()
-            рандомно размешивает коллекции
-
-        Author
-        - String name
-
-        Book
-        - String title
-        - Author author
-        - int pages
-     */
-
-    public static void main(String[] args)
+    public static void main(String[] args) throws SQLException
     {
-        List<TestClass> list = new ArrayList<>();
+        /*System.out.println(BookManager.add(new Book(
+                "mybook",
+                "pa4ok",
+                new Date(),
+                2112,
+                "descriptionefw[pevkwe[gke[w"
+        )));*/
 
-        for(int i=0; i<10; i++) {
-            list.add(new TestClass());
-        }
+        //System.out.println(BookManager.getById(7));
 
-        System.out.println(list);
+        //System.out.println(BookManager.getAll());
 
-        /*
-            есть 2 варианта сортировки
-            первый - мы передаем коллекцию и компаратор
-            подходит в ситуациях, когда есть много разных вариантов сортировки
-            второй - мы передаем только коллекцию
-            но класс хранящийся в коллекции обязан реализовывать интерфейс Comparable
-            подходит, когда коллеция с опр. типом всегда сортируется одинаково
-         */
+        /*Book b7 = BookManager.getById(7);
+        b7.setPages(4444);
+        b7.setCreateDate(new Date());
+        b7.setTitle("b73434434344");
+        BookManager.update(b7);*/
 
-        /*Collections.sort(list, new Comparator<TestClass>() {
-            @Override
-            public int compare(TestClass o1, TestClass o2) {
-                //-1 o1 < o2
-                //0 o1 == o2
-                //1 o1 > o2
-                return Integer.compare(o1.getI(), o2.getI());
-            }
-        });*/
-
-        //тоже самое что и сверху
-        //Collections.sort(list, (o1, o2) -> Integer.compare(o1.getI(), o2.getI()));
-
-        //опять тоже самое
-        //Collections.sort(list, Comparator.comparingInt(TestClass::getI));
-
-        Collections.sort(list);
-        System.out.println(list);
-
-        //рандомно размешивает коллекцию
-        Collections.shuffle(list);
-        System.out.println(list);
-
-        //строки сравниваются через интерфейс Comparable
-        //через вызов метода compareTo у 1 из строк
-        /*Collections.sort(list, new Comparator<TestClass>() {
-            @Override
-            public int compare(TestClass o1, TestClass o2) {
-                String s1 = "efgegfreg";
-                String s2 = "jytkjytu";
-                return s1.compareTo(s2);
-            }
-        });*/
-    }
-}
-
-class TestClass implements Comparable<TestClass>
-{
-    private int i;
-
-    public TestClass(int i) {
-        this.i = i;
+        BookManager.delete(3);
     }
 
-    public TestClass() {
-        this(new Random().nextInt(1000));
-    }
 
-    @Override
-    public int compareTo(TestClass o) {
-        return Integer.compare(this.i, o.getI());
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TestClass testClass = (TestClass) o;
-        return i == testClass.i;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(i);
-    }
-
-    @Override
-    public String toString() {
-        return "TestClass{" +
-                "i=" + i +
-                '}';
-    }
-
-    public int getI() {
-        return i;
-    }
-
-    public void setI(int i) {
-        this.i = i;
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://116.202.236.174:3306/DemoExam?serverTimeZone=UTC&characterEncoding=utf8", "DemoExam", "12345");
     }
 }
