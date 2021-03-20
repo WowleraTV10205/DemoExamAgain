cfc4affa94d8d43d510bbf42502a6b633d5dcdb4
Pa4ok <n_leontiev@mail.ru>
2021-03-20 16:05:41 +0300

    20.03.21-2

--- src/org/brovushka/app/util/CustomTableModel.java
+++ src/org/brovushka/app/util/CustomTableModel.java
@@ -6,9 +6,15 @@ import java.util.List;
 
 public class CustomTableModel<T> extends AbstractTableModel
 {
-    private Class<T> cls;
-    private String[] columnNames;
-    private List<T> rows;
+    /*
+     нельзя путать class и Class
+     class - тип данных впринципе системное слово
+     Class - тип данных который описывает class
+     */
+
+    private final Class<T> cls; //объект который описывает тип данных
+    private final String[] columnNames; //массив который хранит названия колонок
+    private List<T> rows; //коллекция хранящая сущности таблицы
 
     public CustomTableModel(Class<T> cls, String[] columnNames, List<T> rows)
     {
@@ -22,13 +28,19 @@ public class CustomTableModel<T> extends AbstractTableModel
         return rows.size();
     }
 
+    //ограничение: UserEntity или любой другой класс который будет храниться в таблице
+    //не должен содержать лишних полей, вообще, даже статичных
     @Override
     public int getColumnCount() {
+        //получаем массив с зарегистрированнными полями в UserEntity и берем длину
         return cls.getDeclaredFields().length;
     }
 
     @Override
     public Class<?> getColumnClass(int columnIndex) {
+        //мы соотносим номер колонки в таблице с номером поля в классе
+        //получаем также массив всех зарегистрированных полей
+        //выбираем поле по номеру колонки и получаем Class этого поля
         return cls.getDeclaredFields()[columnIndex].getType();
     }
 
@@ -39,16 +51,20 @@ public class CustomTableModel<T> extends AbstractTableModel
 
     @Override
     public String getColumnName(int column) {
-        return columnNames[column];
+        return column >= columnNames.length ? "NAME" : columnNames[column];
     }
 
     @Override
-    public Object getValueAt(int rowIndex, int columnIndex)
-    {
+    public Object getValueAt(int rowIndex, int columnIndex) {
         try {
+            //получаем поле UserEntity по номеру колонки
             Field field = cls.getDeclaredFields()[columnIndex];
+
+            //делаем его доступным (если оно private)
             field.setAccessible(true);
-            return field.get(rows.get(rowIndex));
+
+            //получаем значение этого поля из объекта
+            return field.get(this.rows.get(rowIndex));
         } catch (IllegalAccessException e) {
             e.printStackTrace();
             return "ERROR";
